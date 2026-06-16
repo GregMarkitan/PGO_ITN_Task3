2bad9bd7bc3beefe9c6f566cdd48b75bc68c74d9
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-16 16:06:49 +0200

    Created fields and constructor for Order.java

--- Order.java
+++ Order.java
@@ -1,1 +1,16 @@
-PGO_ITN_Task3
+import java.util.ArrayList;
+
+public class Order {
+	private int orderNumber;
+	private CafeCustomer customer;
+	private ArrayList<MenuItem> items;
+	private boolean paid;
+
+	private static int nextOrderNumber = 1;
+
+	public Order(CafeCustomer customer) {
+		this.orderNumber = nextOrderNumber++;
+		this.customer = customer;
+		this.items = new ArrayList<>();
+		this.paid = false;
+    }
