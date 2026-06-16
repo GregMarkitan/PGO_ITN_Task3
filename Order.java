3b0071f87ab057beef62df765f4c4bc280a659cf
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-16 16:13:14 +0200

    Created methods to be used by Order.java

--- Order.java
+++ Order.java
@@ -13,4 +13,29 @@ public class Order {
 		this.customer = customer;
 		this.items = new ArrayList<>();
 		this.paid = false;
-    }
+		}
+
+	public void addItem(MenuItem item) {
+	items.add(item);
+	}
+
+	public double calculateTotal() {
+		double total = 0;
+		for (MenuItem item : items) {
+		total += item.getPrice();
+	}
+	return total;
+	}
+
+	public int countItems() {
+	return items.size();
+	}
+
+	public void markAsPaid() {
+	paid = true;
+	}
+
+	public static int getNextOrderNumber() {
+	return nextOrderNumber;
+	}
+
