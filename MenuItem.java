959209c4f75a4bb32f2c0cf3937c1d0b74ac0cd2
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-16 15:15:23 +0200

    Created a constructor for MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,5 +1,16 @@
-private String code;
-private String name;
-private double price;
-private String category;
-private static int productCount;
+public class MenuItem {
+	private String code;
+	private String name;
+	private double price;
+	private String category;
+
+	private static int productCount;
+
+    	public MenuItem(String code, String name, double price, String category) {
+       	 this.code = code;
+         this.name = name;
+         this.price = price;
+         this.category = category;
+
+        productCount++;
+    }
